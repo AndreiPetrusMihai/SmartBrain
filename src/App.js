1f563dca8bb0c44c682056c0ab1168b2353ed718
AndreiPetrusMihai <68653908+AndreiPetrusMihai@users.noreply.github.com>
2020-09-12 17:56:37 +0300

    Ports

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ class App extends React.Component {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
     .then(response => {
       if(response){
-        fetch('http://localhost:4000/image',{
+        fetch('https://mighty-oasis-44190.herokuapp.com/image',{
           method:'put',
           headers:{'Content-Type':'application/json'},
           body:JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component {
 
 	onSubmitSignIn = () =>{
 
-		fetch('http://localhost:4000/register',{
+		fetch('https://mighty-oasis-44190.herokuapp.com/register',{
 			method:'post',
 			headers:{'Content-Type':'application/json'},
 			body:JSON.stringify({
